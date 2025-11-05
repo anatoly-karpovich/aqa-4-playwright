e461b1c0ca302e433b40713dcef2459f272d5720
Anatoly Karpovich <anatoly.karpovich90@gmail.com>
2025-11-05 01:09:18 +0100

    practice

--- src/data/types/product.types.ts
+++ src/data/types/product.types.ts
@@ -1,5 +1,5 @@
 import { MANUFACTURERS } from "data/salesPortal/products/manufacturers";
-import { ID, IResponseFields } from "./core.types";
+import { ID, IResponseFields, SortOrder } from "./core.types";
 
 export interface IProduct {
   name: string;
@@ -26,4 +26,27 @@ export interface IProductResponse extends IResponseFields {
 
 export interface IProductsResponse extends IResponseFields {
   Products: IProductFromResponse[];
+}
+
+export interface IProductsSortedResponse extends IProductsResponse {
+  total: number;
+  page: number;
+  limit: number;
+  search: string;
+  manufacturer: string[];
+  sorting: {
+    sortField: ProductsSortField;
+    sortOrder: SortOrder;
+  };
+}
+
+export type ProductsSortField = "createdOn" | "manufacturer" | "price" | "name";
+
+export interface IGetProductsParams {
+  manufacturer: MANUFACTURERS[];
+  search: string;
+  sortField: ProductsSortField;
+  sortOrder: SortOrder;
+  page: number;
+  limit: number;
 }
