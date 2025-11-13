5e4d7cb59f44209b6a3ed31f49e701eff5be8e67
Anatoly Karpovich <anatoly.karpovich90@gmail.com>
2025-11-13 20:57:18 +0100

    updated gitignore, added list reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["html"],
+    ["list"],
     [
       "allure-playwright",
       {
