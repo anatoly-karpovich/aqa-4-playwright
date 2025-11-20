bbd5a79fbb0ce9f7ccd545161d7020007cb18d98
Anatoly Karpovich <anatoly.karpovich90@gmail.com>
2025-11-20 20:40:31 +0100

    added qase support

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,6 +39,22 @@ export default defineConfig({
         },
       },
     ],
+    [
+      "playwright-qase-reporter",
+      {
+        testops: {
+          api: {
+            token: "7bc6431101d2f0c9b7e4a8e1536524e1f09b168db9eb4355384347aea555b18d",
+          },
+          project: "AP",
+          uploadAttachments: true,
+          run: {
+            complete: true,
+            title: "Playwright Test Run" + new Date().toISOString(),
+          },
+        },
+      },
+    ],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
